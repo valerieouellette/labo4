59dad2ac72d77b1fc8df26275169e4ec9cc617b2
valerieo <valerieouellette@icloud.com>
2022-10-04 11:22:33 -0400

    classe Tournoi et Logiciel Magasin

--- valerieouellette_labo4.py
+++ valerieouellette_labo4.py
@@ -1,42 +1,55 @@
 class Jeu:
+
+    NOM = ""
     
     def __init__(self):
-        self.contenu = ""
+        self.prix = 0
+        self.description = ""
+
+    def __str__(self) -> str:
+        return f"{Jeu.NOM}, {self.description}, {self.prix}"
 
 class JeuCartes(Jeu):
 
     NOM = "cartes"
 
     def __init__(self):
-        pass
+        super().__init__()
+        self.prix = 10
+        self.description = "Jeu de 54 cartes classique"
+        self.pochette = PochettePlastique()
+    
+    def achat(self):
+        couleur_pochette = input("Quel couleur pour la pochette? ")
+        self.pochette.couleur = couleur_pochette
 
 class JeuStategie(Jeu):
     
     NOM = "stategie"
 
     def __init__(self):
-        pass
+        super().__init__()
 
 class JeuRole(Jeu):
     
     NOM = "role"
 
     def __init__(self):
-        pass
+        super().__init__()
 
 class JeuMemoire(Jeu):
     
     NOM = "memoire"
 
     def __init__(self):
-        pass
+        super().__init__()
 
 class JeuDes(Jeu):
     
     NOM = "des"
 
     def __init__(self):
-        pass
+        super().__init__()
 
 class PochettePlastique:
     
@@ -45,8 +58,27 @@ class PochettePlastique:
 
 class Tournoi:
 
-    def __init__(self):
-        self.liste_tournois = []
+    LISTE_TOURNOIS = []
+
+    def __init__(self, nom, date):
+        self.nom = nom
+        self.date = date
+        self.participants = []
+    
+    def __str__(self) -> str:
+        return f"{self.nom}: {self.date}"
+    
+    @staticmethod
+    def inscription_tournoi():
+        nom = input("Entrez votre nom: ")
+        courriel = input("Entrez votre courriel: ")
+        if len(Tournoi.LISTE_TOURNOIS) == 0:
+            tournoi = Tournoi()
+            Tournoi.LISTE_TOURNOIS.append(tournoi)
+            tournoi.participants.append((nom, courriel))
+        else:
+            pass
+
 
 class Documentation:
 
@@ -57,3 +89,32 @@ class LogicielMagasin:
     
     def __init__(self):
         self.inventaire = []
+        self.inventaire_quantite = {}
+        self.telecharger_bd()
+        self.quantite_inventaire()
+    
+    def telecharger_bd(self):
+        f = open("data.txt", "r", encoding="utf-8")
+        lignes = f.readlines()
+        for ligne in lignes:
+            ligne = ligne.split(",")
+            nom = ligne[0]
+            if nom == "cartes":
+                self.inventaire.append(JeuCartes())
+            elif nom == "stategie":
+                self.inventaire.append(JeuStategie())
+            elif nom == "role":
+                self.inventaire.append(JeuRole())
+            elif nom == "memoire":
+                self.inventaire.append(JeuMemoire())
+            elif nom == "des":
+                self.inventaire.append(JeuDes())
+        f.close()
+    
+    def quantite_inventaire(self):
+        for jeu in self.inventaire:
+            if jeu in self.inventaire_quantite:
+                self.inventaire_quantite[jeu] += 1
+            else:
+                self.inventaire_quantite[jeu] = 1
+    
