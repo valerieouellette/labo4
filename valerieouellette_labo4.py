05ea2d55508501581cc2b4ea5ee390ec595711ee
valerieo <valerieouellette@icloud.com>
2022-10-03 21:19:27 -0400

    création des classes

---
+++ valerieouellette_labo4.py
@@ -0,0 +1,47 @@
+class Jeu:
+    pass
+
+class JeuCartes:
+
+    NOM = "cartes"
+
+    def __init__(self):
+        pass
+
+class JeuStategie:
+    
+    NOM = "stategie"
+
+    def __init__(self):
+        pass
+
+class JeuRole:
+    
+    NOM = "role"
+
+    def __init__(self):
+        pass
+
+class JeuMemoire:
+    
+    NOM = "memoire"
+
+    def __init__(self):
+        pass
+
+class JeuDes:
+    
+    NOM = "des"
+
+    def __init__(self):
+        pass
+
+class PochettePlastique:
+    
+    def __init__(self):
+        pass
+
+class LogicielMagasin:
+    
+    def __init__(self):
+        self.inventaire = []
